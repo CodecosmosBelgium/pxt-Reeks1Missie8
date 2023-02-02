369d774060ddd92607d884d8a909c9c46a6f9163
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-02-02 10:18:59 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
+let x = 0
+let y = 0
+let z = 0
 loops.forever(function () {
-    if (blocks.testForBlock(AIR, positions.add(
-    agent.getPosition(),
-    world(0, -1, 0)
-    ))) {
+    x = agent.getPosition().getValue(Axis.Y)
+    y = agent.getPosition().getValue(Axis.Y) - 1
+    z = agent.getPosition().getValue(Axis.Y)
+    if (blocks.testForBlock(AIR, world(x, y, z))) {
         agent.move(DOWN, 1)
     }
 })
