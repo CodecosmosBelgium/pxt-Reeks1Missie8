aeecbdc9cff6604f4e673a22441991054b330389
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-02-02 14:17:14 +0100

    pxt.json, custom_functions.ts bijwerken

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,11 +1,7 @@
-
-
 //% color="#D83B01" weight=100 block="AgentExtension"
 namespace AgentExtension {
-
-
     //% block="agent beweeg $dir met $amount"
-    export function checkLevel(dir:SixDirection, amount:number) {
+    export function move(dir:SixDirection, amount:number) {
         let x = agent.getPosition().getValue(Axis.X)
         let y = agent.getPosition().getValue(Axis.Y) - 1
         let z = agent.getPosition().getValue(Axis.Z)
@@ -13,6 +9,18 @@ namespace AgentExtension {
             agent.move(dir, amount)
         } else {
             agent.move(SixDirection.Down, 5)
+        }
+    }
+}
+
+//% color=190 weight=100 block="CodeCosmos"
+namespace CodeCosmos {
+    //% block="controleerLevel"
+    export function checkLevel() {
+        if(agent.getPosition().toString()===world(161,112,347).toString()) {
+            player.execute("function levels/done")
+        } else {
+            player.execute("function levels/wrong")
         }
     }
 
