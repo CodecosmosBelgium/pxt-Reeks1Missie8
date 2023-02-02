ad8bcb87e370e589447ada619d64905dfdf79b55
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-02-02 14:48:09 +0100

    custom_functions.ts bijwerken

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,7 +1,7 @@
 //% color="#D83B01" weight=100 block="AgentExtension"
 namespace AgentExtension {
     //% block="agent beweeg $dir met $amount"
-    export function move(dir:SixDirection, amount:number) {
+    export function move(dir:FourDirection, amount:number) {
         for (let i = 0; i < amount; i++) {
             let x = agent.getPosition().getValue(Axis.X)
             let y = agent.getPosition().getValue(Axis.Y) - 1
@@ -18,6 +18,10 @@ namespace AgentExtension {
 
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
+    //% block="zetKlaar"
+    export function start() {
+        agent.teleport(world(153,112,327), CompassDirection.West)
+    }
     //% block="controleerLevel"
     export function checkLevel() {
         if(agent.getPosition().toString()===world(161,112,347).toString()) {
